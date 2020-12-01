d40e499b8a3c16fdc502d50c8f80fc6f4d79cfcb
Michelle Kondo <michellekondo@Michelles-MacBook-Air.local>
2020-12-01 01:05:27 -0500

    solved day 1!

---
+++ day1.py
@@ -0,0 +1,32 @@
+with open('input.txt') as fin:
+    puzzleinput = fin.read()
+
+
+def parse(data):
+    x = [int(x) for x in data.split('\n')]
+    return x
+
+
+a = parse(puzzleinput)
+
+
+def part_1(L):
+    for x in L:
+        diff = 2020 - x
+        if diff in L:
+            return diff * x
+
+
+print(part_1(a))
+
+
+def part_2(L):
+    for x in L:
+        diff = 2020 - x
+        for y in L:
+            diffA = diff - y
+            if diffA in L:
+                return diffA * y * x
+
+
+print(part_2(a))
